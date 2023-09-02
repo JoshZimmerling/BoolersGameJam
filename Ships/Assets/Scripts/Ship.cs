cd4b94414b791b36321e9c57e31928b4d5e26675
Josh <joshua.zimmerling@gmail.com>
2023-09-01 22:10:08 -0400

    Adding in ship stats

--- Ships/Assets/Scripts/Ship.cs
+++ Ships/Assets/Scripts/Ship.cs
@@ -17,18 +17,65 @@ public class Ship : MonoBehaviour
     }
     [SerializeField] shipTypes shipType;
 
-    [SerializeField] ShipMovement moveController;
-    [SerializeField] Ship_Shooting shootController;
+    float shipHP;
+    float shipSpeed;
+    float shipCost;
 
+    ShipMovement moveController;
+    Ship_Shooting shootController;
 
     private void Start()
     {
         moveController = GetComponent<ShipMovement>();
         shootController = GetComponent<Ship_Shooting>();
+
+        switch (shipType)
+        {
+            case (Ship.shipTypes.Destroyer):
+                shipHP = 50f;
+                shipSpeed = 3f;
+                shipCost = 20f;
+                break;
+            case (Ship.shipTypes.Hawk):
+                shipHP = 20f;
+                shipSpeed = 5f;
+                shipCost = 20f;
+                break;
+            case (Ship.shipTypes.Challenger):
+                shipHP = 60f;
+                shipSpeed = 3f;
+                shipCost = 20f;
+                break;
+            case (Ship.shipTypes.Goliath):
+                shipHP = 150f;
+                shipSpeed = 3f;
+                shipCost = 35f;
+                break;
+            case (Ship.shipTypes.Lightning):
+                shipHP = 20f;
+                shipSpeed = 5f;
+                shipCost = 35f;
+                break;
+            case (Ship.shipTypes.Drone):
+                shipHP = 40f;
+                shipSpeed = 5f;
+                shipCost = 5f;
+                break;
+            case (Ship.shipTypes.Scout):
+                shipHP = 10f;
+                shipSpeed = 6f;
+                shipCost = 10f;
+                break;
+        }
     }
 
     public void setDestination(float x, float y)
     {
         moveController.setTargetDestination(new Vector2(x, y));
+    }
+
+    public shipTypes getShipType()
+    {
+        return shipType;
     }
 }
